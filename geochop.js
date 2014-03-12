892aa7e7c5904aea9ac6d51be092a6e71865e8dd
Travis Pinney <travis.pinney@gmail.com>
2014-03-12 10:59:38 -0400

    Update with .js

--- geochop.js
+++ geochop.js
@@ -32,7 +32,7 @@ if (argv.f) {
   
 
 } else { 
-  console.log("Usage: unzip -p allCountries.zip | node geochop -f <filterfile>");
+  console.log("Usage: unzip -p allCountries.zip | node geochop.js -f <filterfile>");
   console.log("<filterfile> contains a GeoJSON geometry to filter on");
 }
 
